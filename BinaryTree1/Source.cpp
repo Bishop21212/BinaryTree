b7fdcb314260c28d5cbfa70620c935575672aac2
Bishop21212 <91216505+Bishop21212@users.noreply.github.com>
2022-03-03 23:40:04 +0500

    Add project files.

---
+++ BinaryTree1/Source.cpp
@@ -0,0 +1,119 @@
+//BinaryTree1
+#include<iostream>
+using namespace std;
+
+class Tree
+{
+	class Element
+	{
+		int Data;
+		Element* pLeft;
+		Element* pRight;
+	public:
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) :Data(Data), pLeft(pLeft), pRight(pRight)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class Tree;
+	}*Root;
+public:
+	Element* getRoot()const
+	{
+		return Root;
+	}
+	Tree()
+	{
+		this->Root = nullptr;
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Tree()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+	void insert(int Data)
+	{
+		insert(Data, Root);
+	}
+	int minValue()const
+	{
+		return minValue(Root);
+	}
+	int maxValue()const
+	{
+		return maxValue(Root);
+	}
+	int size()const
+	{
+		return size(Root);
+	}
+	void print()const
+	{
+		print(Root);
+	}
+private:
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)
+		{
+			this->Root = new Element(Data);
+			return;
+		}
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+	int minValue(Element* Root)const
+	{
+		if (Root == nullptr)return 0;
+		/*if (Root->pLeft == nullptr)return Root->Data;
+		else return minValue(Root->pLeft);*/
+		//return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+		return Root->pLeft ? minValue(Root->pLeft) : Root->Data;
+	}
+	int maxValue(Element* Root)const
+	{
+		if (Root == nullptr)return 0;
+		return Root->pRight ? maxValue(Root->pRight) : Root->Data;
+	}
+
+	int size(Element* Root)const
+	{
+		return Root == nullptr ? 0 : size(Root->pLeft) + size(Root->pRight) + 1;
+	}
+	void print(Element* Root)const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << "\t";
+		print(Root->pRight);
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер дерева: "; cin >> n;
+	Tree tree;
+	for (int i{}; i < n; ++i)
+	{
+		tree.insert(rand() % 100);
+	}
+	tree.print();
+	cout << endl;
+	cout << "Минимальное значение в дереве: " << tree.minValue() << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue() << endl;
+	cout << "Количество элементов дерева: " << tree.size() << endl;
+}
